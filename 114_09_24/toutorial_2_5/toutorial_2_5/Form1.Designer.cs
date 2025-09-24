5a23769e35356b5c26351deca68b51a4c25091b7
Ulrica-55 <a114221055@mail.shu.edu.tw>
2025-09-24 12:58:27 +0800

    114_09_24

---
+++ 114_09_24/toutorial_2_5/toutorial_2_5/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace toutorial_2_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cardbackPictureBox = new System.Windows.Forms.PictureBox();
+            this.cardfacePictureBox = new System.Windows.Forms.PictureBox();
+            this.showfaceButton = new System.Windows.Forms.Button();
+            this.showbackButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbackPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardfacePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cardbackPictureBox
+            // 
+            this.cardbackPictureBox.Image = global::toutorial_2_5.Properties.Resources.Backface_Red;
+            this.cardbackPictureBox.Location = new System.Drawing.Point(320, 66);
+            this.cardbackPictureBox.Name = "cardbackPictureBox";
+            this.cardbackPictureBox.Size = new System.Drawing.Size(137, 258);
+            this.cardbackPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardbackPictureBox.TabIndex = 1;
+            this.cardbackPictureBox.TabStop = false;
+            // 
+            // cardfacePictureBox
+            // 
+            this.cardfacePictureBox.Image = global::toutorial_2_5.Properties.Resources.Ace_Hearts;
+            this.cardfacePictureBox.Location = new System.Drawing.Point(320, 66);
+            this.cardfacePictureBox.Name = "cardfacePictureBox";
+            this.cardfacePictureBox.Size = new System.Drawing.Size(137, 258);
+            this.cardfacePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardfacePictureBox.TabIndex = 0;
+            this.cardfacePictureBox.TabStop = false;
+            // 
+            // showfaceButton
+            // 
+            this.showfaceButton.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.showfaceButton.Location = new System.Drawing.Point(104, 340);
+            this.showfaceButton.Name = "showfaceButton";
+            this.showfaceButton.Size = new System.Drawing.Size(214, 48);
+            this.showfaceButton.TabIndex = 2;
+            this.showfaceButton.Text = "顯示正面";
+            this.showfaceButton.UseVisualStyleBackColor = true;
+            this.showfaceButton.Click += new System.EventHandler(this.showfaceButton_Click);
+            // 
+            // showbackButton
+            // 
+            this.showbackButton.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.showbackButton.Location = new System.Drawing.Point(441, 340);
+            this.showbackButton.Name = "showbackButton";
+            this.showbackButton.Size = new System.Drawing.Size(192, 52);
+            this.showbackButton.TabIndex = 3;
+            this.showbackButton.Text = "顯示背面";
+            this.showbackButton.UseVisualStyleBackColor = true;
+            this.showbackButton.Click += new System.EventHandler(this.showbackButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showbackButton);
+            this.Controls.Add(this.showfaceButton);
+            this.Controls.Add(this.cardbackPictureBox);
+            this.Controls.Add(this.cardfacePictureBox);
+            this.Name = "Form1";
+            this.Text = "翻轉撲克牌";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cardbackPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardfacePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cardfacePictureBox;
+        private System.Windows.Forms.PictureBox cardbackPictureBox;
+        private System.Windows.Forms.Button showfaceButton;
+        private System.Windows.Forms.Button showbackButton;
+    }
+}
+
---
+++ 114_09_24/toutorial_2_5/toutorial_2_5/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace toutorial_2_5
+{
+    public partial class Form1 : Form
+    {
+        // 建構子：初始化表單元件
+        public Form1()
+        {
+            InitializeComponent(); // 初始化表單上的所有控制項
+        }
+
+        // 表單載入事件處理函式
+        // 當表單載入時，將卡牌正面與背面圖片隱藏
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cardbackPictureBox.Visible = false; // 隱藏卡牌背面圖片
+            cardfacePictureBox.Visible = false; // 隱藏卡牌正面圖片
+        }
+
+        // 顯示卡牌正面按鈕的事件處理函式
+        // 當使用者按下顯示正面按鈕時，顯示正面圖片並隱藏背面圖片
+        private void showfaceButton_Click(object sender, EventArgs e)
+        {
+            cardbackPictureBox.Visible = false; // 隱藏卡牌背面圖片
+            cardfacePictureBox.Visible = true;  // 顯示卡牌正面圖片
+        }
+
+        // 顯示卡牌背面按鈕的事件處理函式
+        // 當使用者按下顯示背面按鈕時，顯示背面圖片並隱藏正面圖片
+        private void showbackButton_Click(object sender, EventArgs e)
+        {
+            cardbackPictureBox.Visible = true;  // 顯示卡牌背面圖片
+            cardfacePictureBox.Visible = false; // 隱藏卡牌正面圖片
+        }
+    }
+}
